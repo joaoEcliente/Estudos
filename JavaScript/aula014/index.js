4632aeadf546abca64ef0e7ec9d64c43e4e2aeab
João Damasio <joao.vitor@ecliente.com.br>
2026-07-27 11:31:00 -0300

    Aula sobre events

---
+++ JavaScript/aula014/index.js
@@ -0,0 +1,23 @@
+//onclick
+function clicar(){
+    alert('Clicou');
+}
+
+//ondblclick
+
+function duplo_click(){
+    alert('Pressionou 2 vezes o mouse')
+}
+//onmousedown
+function pressionado(){
+    alert('pressionado');
+}
+//onmouseover
+function movido_para_cima(){
+    alert('movido para cima');
+}
+//onmouseout
+
+function movido_para_fora(){
+    alert('Você moveu o ponteiro para fora do container.')
+}
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Estudos
-
-Repositório criado para estudos e práticas de desenvolvimento.
