8c57fd27af1800ebbfe616d0f15479cdca498a5f
João Damasio <joao.vitor@ecliente.com.br>
2026-07-27 08:19:25 -0300

    Aula sobre váriaveis JavaScript

---
+++ JavaScript/aula002/index.js
@@ -0,0 +1,20 @@
+//Aula sopbre Váriaveis
+
+//Strings
+
+var nome = 'João Damasio';
+
+//Float
+
+var numFloat = 10.5;
+
+// Int
+
+var num = 10;
+
+// Boolean
+
+var teste = true;
+
+document.write(nome);
+console.log(`Olá mundo tenho: `, num, `Anos.`);
