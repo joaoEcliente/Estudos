5339a3e4319977cba2b3d8cbb42c57206aabceb9
João Damasio <joao.vitor@ecliente.com.br>
2026-07-27 09:47:39 -0300

    AUla sobre switch, uma condição só que limitada, ainda mais recomendado o uso de if/else

---
+++ JavaScript/aula008/index.js
@@ -0,0 +1,18 @@
+var opcao = 3;
+
+switch(opcao){
+    case 1:
+        document.write('Opção 1 selecionada.');
+
+        break;
+
+    case 2:
+        document.write('Opção 2 selecionada.');
+
+        break;
+
+    case 3:
+        document.write('Opção 3 selecionada.');
+
+        break;
+}
